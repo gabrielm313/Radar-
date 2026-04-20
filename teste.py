2a265362de61f747b65029c9bc414913e6ab7519
gabrielm313 <gabrielsm9@al.insper.edu.br>
2026-04-20 02:26:39 -0300

    commit 2

--- teste.py
+++ teste.py
@@ -1,20 +1,20 @@
 import cv2
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(0) #iniciar câmera, 0 significa que é uma câmera integrada à maquina.
 
 while True:
-    ret, frame = cap.read()
+    ret, frame = cap.read() #ret é um boleano que indica se o frame foi capturado com sucesso ou não.
 
-    if not ret:
+    if not ret: #se o frame foi capturado com sucesso, então...
         print('Câmera não encontrada')
         False
     
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) #cvtcolor altera a cor dos frames
 
-    cv2.imshow('Gravando', frame)
+    cv2.imshow('Gravando', frame) #tela que aparece o vídeo da gravação
 
-    if cv2.waitKey(1) & 0xFF == ord('x'):
+    if cv2.waitKey(1) & 0xFF == ord('x'): #condicional para parar de "gravar", clicando no 'x'
         break
 
-cap.release()
-cv2.destroyAllWindows()
+cap.release() #encerra o uso da câmera no código
+cv2.destroyAllWindows() #fecha todas janelas abertas pelo código
