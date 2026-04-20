eb5dbbbdec6da79f1076578d720911b081820605
gabrielm313 <gabrielsm9@al.insper.edu.br>
2026-04-20 02:19:30 -0300

    teste câmera

--- teste.py
+++ teste.py
@@ -1,1 +1,20 @@
-print('Hello World')
+import cv2
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+
+    if not ret:
+        print('Câmera não encontrada')
+        False
+    
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    cv2.imshow('Gravando', frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('x'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
