9fec930fa09fe2521146eb6307d1f88672718ee3
Carlos Eduardo Carvalho Sousa <carlosecs4@al.insper.edu.br>
2026-04-21 01:26:04 -0300

    colocando um script teste que reconhece objetos com a IA

--- teste.py
+++ teste.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 
 cap = cv2.VideoCapture(0) #iniciar câmera, 0 significa que é uma câmera integrada à maquina.
 
@@ -14,6 +15,9 @@ while True:
     cv2.imshow('Gravando', frame) #tela que aparece o vídeo da gravação
 
     if cv2.waitKey(1) & 0xFF == ord('x'): #condicional para parar de "gravar", clicando no 'x'
+        break
+
+    if cv2.getWindowProperty("Câmera de Cinto com IA", cv2.WND_PROP_VISIBLE) < 1:
         break
 
 cap.release() #encerra o uso da câmera no código
